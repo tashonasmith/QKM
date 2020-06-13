5648e8cd3e3b2da5ed757d8150fb8b06dae5ebe1
aj mac <macdonalda47@gmail.com>
2020-06-13 16:17:23 -0500

    insert drafted code for models

--- models/example.js
+++ models/example.js
@@ -1,7 +1,52 @@
 module.exports = function(sequelize, DataTypes) {
-  var Example = sequelize.define("Example", {
-    text: DataTypes.STRING,
-    description: DataTypes.TEXT
-  });
-  return Example;
+    var Example = sequelize.define("Example", {
+        text: DataTypes.STRING,
+        description: DataTypes.TEXT
+    });
+    return Example;
 };
+
+
+//AJ's draft 
+// module.exports = (sequelize, DataTypes) => {
+//     var Kids = sequelize.define('Kids', {
+//         title: DataTypes.STRING
+//     });
+
+//     Kids.associate = function(models) {
+//         models.Kids.belongsTo(models.User, {
+//             onDelete: "CASCADE",
+//             foreignKey: {
+//                 allowNull: false
+//             }
+//         });
+//     };
+
+//     return Kids;
+// };
+
+
+// module.exports = (sequelize, DataTypes) => {
+//         var Timedhw = sequelize.define('Timedhw', {
+//             title: DataTypes.STRING
+//         });
+
+//         //code to put foreign key on the “belongsTo” tables
+//         Timedhw.associate = function(models) {
+//                 models.Timedhw.belongsTo(models.Kids, {
+//                         onDelete: "CASCADE",
+//                         foreignKey: {
+//                             allowNull: false
+//                         }
+
+//                         models.Timedhw.belongsTo(models.Users, {
+//                             onDelete: "CASCADE",
+//                             foreignKey: {
+//                                 allowNull: false
+//                             }
+
+//                         });
+//                     };
+
+//                     return Timedhw;
+//                 };
