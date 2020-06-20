99c07ec455cd0d10a36bf6126088e6475e501215
Tashona Smith <tashona.smith@gmail.com>
2020-06-20 14:23:04 -0500

    JS and api routes for Done buttons on individualkid page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,5 @@
 var db = require("../models");
 
-
 // module.exports = function(app) {
 //   // Get all examples
 //   app.get("/api/examples", function(req, res) {
@@ -47,7 +46,76 @@ module.exports = function(app) {
         });
 
     });
-  }) 
+  });
+  app.get("/api/Timedhw/", function (req, res) {
+    db.Timedhw.findAll({})
+    .then(function(dbTimedhw) {
+      res.json(dbTimedhw);
+      console.log(dbTimedhw);
+    })
+  });
+  app.get("/api/Timedhw/:id", function (req, res) {
+    db.Timedhw.findAll({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(function(dbTimedhwid) {
+      res.json(dbTimedhwid);
+      console.log(dbTimedhwid);
+    })
+  });
+  app.delete("/api/kids/:id/Timedhw/:timedhwid", function (req, res) {
+    console.log(req.params.id)
+    db.Timedhw.destroy({
+      where: {
+        id: req.params.timedhwid
+      }
+    }).then(function(dbTimedhw) {
+      res.json(dbTimedhw)
+      console.log("Successfully deleted")
+    });
+  }); 
+  app.delete("/api/kids/:id/Taskedhw/:taskedhwid", function (req, res) {
+    db.Taskedhw.destroy({
+      where: {
+        id: req.params.taskedhwid
+      }
+    }).then(function(dbTaskedhw) {
+      res.json(dbTaskedhw);
+      console.log("Task successfully deleted")
+    });
+  });
+  app.delete("/api/kids/:id/Chores/:choreid", function (req, res) {
+    db.Chores.destroy({
+      where: {
+        id: req.params.choreid
+      }
+    }).then(function(dbChores) {
+      res.json(dbChores);
+      console.log("Chore successfully deleted")
+    });
+  });
+  app.delete("/api/kids/:id/Dietary/:foodid", function (req, res) {
+    db.Dietary.destroy({
+      where: {
+        id: req.params.foodid
+      }
+    }).then(function(dbDietary) {
+      res.json(dbDietary);
+      console.log("Food successfully deleted")
+    });
+  });
+  app.post("/api/Timedhw", function(req, res) {
+    console.log(req.body)
+    db.Timedhw.create({
+      assignment: req.body.assignment,
+      minutes_required: req.body.minutes_required,
+      UserId: req.body.user_id
+    }).then(function(dbTimedhw) {
+      res.json(dbTimedhw);
+    });
+  });
 }
 
 
