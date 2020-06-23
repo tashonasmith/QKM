15b404fd66be605a434769b09ad5bef448b1c714
Tashona Smith <tashona.smith@gmail.com>
2020-06-22 21:52:58 -0500

    Change config.json to config.js.  Create html route for individualkid.handlebars

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,6 @@
 var db = require("../models");
 
-// module.exports = function(app) {
-//   // Get all examples
-//   app.get("/api/examples", function(req, res) {
-//     db.Example.findAll({}).then(function(dbExamples) {
-//       res.json(dbExamples);
-//     });
-//   });
 
-//   // Create a new example
-//   app.post("/api/examples", function(req, res) {
-//     db.Example.create(req.body).then(function(dbExample) {
-//       res.json(dbExample);
-//     });
-//   });
-
-//   // Delete an example by id
-//   app.delete("/api/examples/:id", function(req, res) {
-//     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-//       res.json(dbExample);
-//     });
-//   });
-// };
-
-
-//change to html route?
 module.exports = function(app) {
   // app.get("/api/kids/:id", function(req, res) {
   //   db.Kids.findOne({
